f02fe9d4a9c171d091a8c5bca294ff9f9f657eb6
Raashid Mahdi <36306773+raashidmahdi1997@users.noreply.github.com>
2018-10-29 15:07:18 +0600

    Update Rest/v1/PHPMailer/quary.sql
    Price update queary done

--- Rest/v1/PHPMailer/quary.sql
+++ Rest/v1/PHPMailer/quary.sql
@@ -3,3 +3,41 @@ SET price.price = 110
 where price.book_id = 1
 and price.quality_id = 1
 
+
+
+
+/* Book price update  */
+UPDATE price
+SET price.price = price.price + 10
+WHERE price.price <= 120
+
+/*                   */
+
+UPDATE price
+SET price.price = price.price + 15
+WHERE price.price >= 130
+and price.price <= 220
+
+
+/*        */
+
+UPDATE price
+SET price.price = price.price + 20
+WHERE price.price > 220
+and price.price <= 500
+
+/*    */
+
+UPDATE price
+SET price.price = price.price + 10
+WHERE price.price > 500
+
+
+
+
+
+/* Inseerting into cartitem  */
+
+INSERT INTO cartitem 
+(cartitem.item_id,cartitem.book_id,cartitem.book_order_id,cartitem.price_id,cartitem.promo_id)
+VALUES (NULL,1,(SELECT MAX(bookorder.book_order_id) FROM bookorder),1,1)
