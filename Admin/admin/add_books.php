45be7ba6ac89108d7fb7c45b011c659f909982dc
Raashid Mahdi <36306773+raashidmahdi1997@users.noreply.github.com>
2018-10-30 16:17:51 +0600

    Update Admin/admin/add_books.php
    AWSAF, check kor. same nam er boi insert hobe na ar in sha allah. uppercase lowercase solved..tui ektu checking chala ...

--- Admin/admin/add_books.php
+++ Admin/admin/add_books.php
@@ -24,7 +24,7 @@
 
     <!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv.min.js"></script> -->
 
-     <?php 
+    <?php 
      include $_SERVER['DOCUMENT_ROOT'].'/../phpincludes/auth.php';
     require_once '../include/DbHandler.php';
     require_once '../include/PassHash.php';
@@ -80,72 +80,73 @@
     ?>
 
 </head>
+
 <body>
-        <!-- Left Panel -->
+    <!-- Left Panel -->
 
 
-        <aside id="left-panel" class="left-panel">
-            <nav class="navbar navbar-expand-sm navbar-default">
+    <aside id="left-panel" class="left-panel">
+        <nav class="navbar navbar-expand-sm navbar-default">
 
-                <div class="navbar-header">
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
-                        <i class="fa fa-bars"></i>
-                    </button>
-                    <a class="navbar-brand" href="./"><img src="images/logo.png" alt="Logo"></a>
-                    <a class="navbar-brand hidden" href="./"><img src="images/logo2.png" alt="Logo"></a>
+            <div class="navbar-header">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="./"><img src="images/logo.png" alt="Logo"></a>
+                <a class="navbar-brand hidden" href="./"><img src="images/logo2.png" alt="Logo"></a>
+            </div>
+
+            <div id="main-menu" class="main-menu collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active">
+                        <a href="home.php"> <i class="menu-icon fa fa-dashboard"></i>Dashboard </a>
+                    </li>
+                    <li>
+                        <!-- <a href="new-post.php"> <i class="menu-icon fa fa-th"></i>New Post </a> -->
+                        <a href="book_info.php"> <i class="menu-icon fa fa-area-chart"></i>Book Info </a>
+                        <a href="add_books.php"> <i class="menu-icon fa fa-area-chart"></i>Add Books </a>
+                        <a href="order.php"> <i class="menu-icon fa fa-bar-chart"></i>Create Order</a>
+                        <a href="delivery_status.php"> <i class="menu-icon fa fa-th"></i>Delivery Status</a>
+                    </li>
+
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </nav>
+    </aside><!-- /#left-panel -->
+
+    <!-- Right Panel -->
+
+    <div id="right-panel" class="right-panel">
+
+        <!-- Header-->
+        <header id="header" class="header">
+
+            <div class="header-menu">
+
+                <div class="col-sm-7">
+                    <a id="menuToggle" class="menutoggle pull-left"><i class="fa fa fa-tasks"></i></a>
+
                 </div>
 
-                <div id="main-menu" class="main-menu collapse navbar-collapse">
-                    <ul class="nav navbar-nav">
-                        <li class="active">
-                            <a href="home.php"> <i class="menu-icon fa fa-dashboard"></i>Dashboard </a>
-                        </li>
-                        <li>
-                            <!-- <a href="new-post.php"> <i class="menu-icon fa fa-th"></i>New Post </a> -->
-                            <a href="book_info.php"> <i class="menu-icon fa fa-area-chart"></i>Book Info </a>
-                            <a href="add_books.php"> <i class="menu-icon fa fa-area-chart"></i>Add Books </a>
-                            <a href="order.php"> <i class="menu-icon fa fa-bar-chart"></i>Create Order</a>
-                            <a href="delivery_status.php"> <i class="menu-icon fa fa-th"></i>Delivery Status</a>
-                            </li>
+                <div class="col-sm-5">
+                    <div class="user-area dropdown float-right">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <img class="user-avatar rounded-circle" src="images/admin.jpg" alt="User Avatar">
+                        </a>
 
-                    </ul>
-                </div><!-- /.navbar-collapse -->
-            </nav>
-        </aside><!-- /#left-panel -->
-
-        <!-- Right Panel -->
-
-        <div id="right-panel" class="right-panel">
-
-            <!-- Header-->
-            <header id="header" class="header">
-
-                <div class="header-menu">
-
-                    <div class="col-sm-7">
-                        <a id="menuToggle" class="menutoggle pull-left"><i class="fa fa fa-tasks"></i></a>
-
-                    </div>
-
-                    <div class="col-sm-5">
-                        <div class="user-area dropdown float-right">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <img class="user-avatar rounded-circle" src="images/admin.jpg" alt="User Avatar">
-                            </a>
-
-                            <div class="user-menu dropdown-menu">
-                                <a class="nav-link" href="#"><i class="fa fa- user"></i>My Profile</a>
-                                <a class="nav-link" href="logout.php"><i class="fa fa-power -off"></i>Logout</a>
-                            </div>
+                        <div class="user-menu dropdown-menu">
+                            <a class="nav-link" href="#"><i class="fa fa- user"></i>My Profile</a>
+                            <a class="nav-link" href="logout.php"><i class="fa fa-power -off"></i>Logout</a>
                         </div>
-
-
-
                     </div>
-                </div>
 
-            </header><!-- /header -->
-            <!-- Header-->
+
+
+                </div>
+            </div>
+
+        </header><!-- /header -->
+        <!-- Header-->
 
 
 
@@ -173,7 +174,7 @@
         <div class="container">
             <div class="row">
                 <div class='col-sm-8'>
-            <?php
+                    <?php
             $err='';
             $_SESSION['message']='';
 
@@ -211,9 +212,24 @@
                 
                 
                 $conn = new mysqli("localhost", "kolpobdc", "5NUl.2tru1T3-H", "kolpobdc_site");
+                
+                $strings ="SELECT book_id FROM Book WHERE LOWER(name) = LOWER('".$book."')";
+                
+                $result = $conn->prepare($strings);
+                $result->execute();
+                $result->bind_result($book_id);
+                $temp_1 = array();
+                
+                while($result->fetch()) {       
+                    $temp_1["book_id"] = $book_id;
+                }
 
-                $strings ="INSERT INTO Book (book_id , name) VALUES (NULL , '".$book."')";
-                $result = $conn->query($strings);
+                $result->close();
+
+                if(sizeof($temp_1) > 0)
+                {
+                    $strings ="INSERT INTO Book (book_id , name) VALUES (NULL , '".$book."')";
+                    $result = $conn->query($strings);
                 
                 $strings ="SELECT book_id FROM Book WHERE name = '".$book."'";
                 
@@ -225,6 +241,8 @@
                 while($result->fetch()) {       
                     $tmp["book_id"] = $book_id;
                 }
+                
+
                 $result->close();
                 
                 $strings ="INSERT INTO BookEdition (id ,book_id , edition_id) VALUES (NULL , '".$tmp["book_id"]."' , '".$Edition."')";
@@ -341,108 +359,117 @@
                 
                 $_SESSION['message']="Add Book Successful!";
                 
+
+                }
+                else
+                {
+                    $_SESSION['message']="BOOK already exists";
+                }
+                
+
+                
             }
             else{
             ?>
 
-<form action="add_books.php" method="post">
-  <div class="form-group">
-    <label>Book Name</label>
-    <input type="text" class="form-control" aria-describedby="Book Name" name="name" placeholder="Book Name"><br>
-    <small id="emailHelp" class="form-text text-muted">Only Enter the book Name</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputEdition">Edition</label>
-    <select name="Edition" class="form-control">
-        <option>Select Edition.<option>
-        <option></option>
-        <option value="1">1st Edition</option>
-        <option value="2">2nd Edition</option>
-        <option value="3">3rd Edition</option>
-        <option value="4">4th Edition</option>
-        <option value="5">5th Edition</option>
-        <option value="6">6th Edition</option>
-        <option value="7">7th Edition</option>
-        <option value="8">8th Edition</option>
-        <option value="9">9th Edition</option>
-        <option value="10">10th Edition</option>
-        <option value="11">11th Edition</option>
-        <option value="12">12th Edition</option>
-        <option value="13">13th Edition</option>
-        <option value="14">14th Edition</option>
-        <option value="15">15th Edition</option>
-        <option value="16">16th Edition</option>
-        <option value="17">17th Edition</option>
-        <option value="18">18th Edition</option>
-        <option value="19">19th Edition</option>
-        <option value="20">20th Edition</option>
+                    <form action="add_books.php" method="post">
+                        <div class="form-group">
+                            <label>Book Name</label>
+                            <input type="text" class="form-control" aria-describedby="Book Name" name="name" placeholder="Book Name"><br>
+                            <small id="emailHelp" class="form-text text-muted">Only Enter the book Name</small>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEdition">Edition</label>
+                            <select name="Edition" class="form-control">
+                                <option>Select Edition.<option>
+                                <option></option>
+                                <option value="1">1st Edition</option>
+                                <option value="2">2nd Edition</option>
+                                <option value="3">3rd Edition</option>
+                                <option value="4">4th Edition</option>
+                                <option value="5">5th Edition</option>
+                                <option value="6">6th Edition</option>
+                                <option value="7">7th Edition</option>
+                                <option value="8">8th Edition</option>
+                                <option value="9">9th Edition</option>
+                                <option value="10">10th Edition</option>
+                                <option value="11">11th Edition</option>
+                                <option value="12">12th Edition</option>
+                                <option value="13">13th Edition</option>
+                                <option value="14">14th Edition</option>
+                                <option value="15">15th Edition</option>
+                                <option value="16">16th Edition</option>
+                                <option value="17">17th Edition</option>
+                                <option value="18">18th Edition</option>
+                                <option value="19">19th Edition</option>
+                                <option value="20">20th Edition</option>
 
-    </select>
-    <!-- <input type="number" name="Edition" class="form-control" placeholder="Edition"><br> -->
-  </div>
-    <div class="form-group">
-    <select name="dept" class="form-control">
-        <option>Select Department</option>
-        <option value="1">CSE</option>
-        <option value="2">EEE</option>
-        <option value="3">Chemical</option>
-        <option value="4">BME</option>
-        <option value="5">ME</option>
-        <option value="6">IPE</option>
-        <option value="7">Architecture</option>
-        <option value="8">Civil</option>
-        <option value="9">MME</option>
-        <option value="10">NAME</option>
-        <option value="11">URP</option>
-        <option value="12">WRE</option>
-    </select>
-    </div>
-    <div class="form-group">
-    <select name="sem" class="form-control">
-        <option value="1">L1 T1</option>
-        <option value="2">L1 T2</option>
-        <option value="3">L2 T1</option>
-        <option value="4">L2 T2</option>
-        <option value="5">L3 T1</option>
-        <option value="6">L3 T2</option>
-        <option value="7">L4 T1</option>
-        <option value="8">L4 T2</option>
-        <option value="9">L5 T1</option>
-        <option value="10">L5 T2</option>
-    </select>
-        
-    </div>
-    <div class="form-group">
-        <label>Price White Print</label>
-        <input type="number" name="Price_W" class="form-control" placeholder="Price White Print">
-    </div>
-    <div class="form-group">
-        <input type="number" name="Price_N" class="form-control" placeholder="Price News Print"><br>
-    </div>
-    <div class="form-group">
-        <input class="form-control" type="number" name="Price_O" placeholder="Price Original Print"><br>
-    </div>
-    <div class="form-group">
-        <label>Best Known Author of Book. (Mandatory)</label>
-        <input type="text" class="form-control" name="author" placeholder="author">
-    </div>
-    <div class="form-group">
-        <input type="text" name="Author_2" class="form-control" placeholder="Author 2"><br>
-    </div>
-    <div class="form-group">
-        <input type="text" name="Author_3" class="form-control" placeholder="Author 3"><br>
-    </div>
-    <div class="form-group">
-        <input type="text" class="form-control" name="Author_4" placeholder="Author 4"><br>
-    </div>
-    <div class="form-group">
-        <input type="text" name="Author_5" class="form-control" placeholder="Author 5"><br>
-    </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+                            </select>
+                            <!-- <input type="number" name="Edition" class="form-control" placeholder="Edition"><br> -->
+                        </div>
+                        <div class="form-group">
+                            <select name="dept" class="form-control">
+                                <option>Select Department</option>
+                                <option value="1">CSE</option>
+                                <option value="2">EEE</option>
+                                <option value="3">Chemical</option>
+                                <option value="4">BME</option>
+                                <option value="5">ME</option>
+                                <option value="6">IPE</option>
+                                <option value="7">Architecture</option>
+                                <option value="8">Civil</option>
+                                <option value="9">MME</option>
+                                <option value="10">NAME</option>
+                                <option value="11">URP</option>
+                                <option value="12">WRE</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <select name="sem" class="form-control">
+                                <option value="1">L1 T1</option>
+                                <option value="2">L1 T2</option>
+                                <option value="3">L2 T1</option>
+                                <option value="4">L2 T2</option>
+                                <option value="5">L3 T1</option>
+                                <option value="6">L3 T2</option>
+                                <option value="7">L4 T1</option>
+                                <option value="8">L4 T2</option>
+                                <option value="9">L5 T1</option>
+                                <option value="10">L5 T2</option>
+                            </select>
 
-                    
-                    
+                        </div>
+                        <div class="form-group">
+                            <label>Price White Print</label>
+                            <input type="number" name="Price_W" class="form-control" placeholder="Price White Print">
+                        </div>
+                        <div class="form-group">
+                            <input type="number" name="Price_N" class="form-control" placeholder="Price News Print"><br>
+                        </div>
+                        <div class="form-group">
+                            <input class="form-control" type="number" name="Price_O" placeholder="Price Original Print"><br>
+                        </div>
+                        <div class="form-group">
+                            <label>Best Known Author of Book. (Mandatory)</label>
+                            <input type="text" class="form-control" name="author" placeholder="author">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="Author_2" class="form-control" placeholder="Author 2"><br>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="Author_3" class="form-control" placeholder="Author 3"><br>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="Author_4" placeholder="Author 4"><br>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="Author_5" class="form-control" placeholder="Author 5"><br>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+
+
+
                     <!-- <input type="text" name="Tag_1" placeholder="Tag"><br>
                     <input type="text" name="Tag_2" placeholder="Tag"><br>
                     <input type="text" name="Tag_3" placeholder="Tag"><br>
@@ -463,15 +490,15 @@
                     <input type="text" name="Tag_18" placeholder="Tag"><br>
                     <input type="text" name="Tag_19" placeholder="Tag"><br>
                     <input type="text" name="Tag_20" placeholder="Tag"><br> -->
-    
-           
-            </div>
-            <?php
+
+
+                </div>
+                <?php
             }
             echo "<h1>".$_SESSION['message']."</h1>";
             ?>
 
-        </div>
+            </div>
 
         </div><!-- .animated -->
     </div><!-- .content -->
@@ -503,10 +530,12 @@
 
     <script type="text/javascript">
         $(document).ready(function() {
-          $('#bootstrap-data-table-export').DataTable();
-        } );
+            $('#bootstrap-data-table-export').DataTable();
+        });
+
     </script>
 
 
 </body>
+
 </html>
